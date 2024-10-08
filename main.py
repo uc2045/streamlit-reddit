a58d9e7fd767af679871957344555f53067b4200
KV2045 <kaushik@kritai.com>
2024-10-07 20:04:56 -0500

    edited

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import streamlit as st
+from google.cloud import firestore
 
-st.header ('Hello world!')
-if st.button('Click here'):
-    st.balloons()
-    st.balloons()
-    st.balloons()
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+# Streamlit widgets to let a user create a new post
+title = st.text_input("Post title")
+url = st.text_input("Post url")
+submit = st.button("Submit new post")
+
+# Once the user has submitted, upload it to the database
+if title and url and submit:
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
+
+# And then render each post, using some light Markdown
+posts_ref = db.collection("posts")
+for doc in posts_ref.stream():
+	post = doc.to_dict()
+	title = post["title"]
+	url = post["url"]
+
+	st.subheader(f"Post: {title}")
+	st.write(f":link: [{url}]({url})")
